fe8df5d4c3ed3150bb3622085a42b9e9762454ba
NellieJosie <nellyjosephine4@gmail.com>
2023-05-30 14:22:58 -0700

    updates

--- handle_print.c
+++ handle_print.c
@@ -12,11 +12,10 @@
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
@@ -32,20 +31,15 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-
 		else if (width)
 		{
 			--(*ind);
-
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-
 			if (fmt[*ind] == ' ')
 				--(*ind);
-
 			return (1);
 		}
 		unknow_len += write(1, &fmt[*ind], 1);
